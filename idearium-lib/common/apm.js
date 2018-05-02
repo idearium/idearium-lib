1bb678bf2520b7e8e052432fab15330b57f58350
Allan Chau <allan.chau@icloud.com>
2018-05-02 11:05:23 +0930

    Capture unhandled promises too
    Signed-off-by: Allan Chau <allan.chau@icloud.com>

--- idearium-lib/common/apm.js
+++ idearium-lib/common/apm.js
@@ -4,6 +4,8 @@ const apm = require('elastic-apm-node');
 
 apm.start();
 
+process.on('unhandledRejection', err => apm.captureError(err));
+
 apm.handleUncaughtExceptions(require('./exception'));
 
 module.exports = apm;
