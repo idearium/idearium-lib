034d4a2ce64fa0e078f83e751de81de8a69c29c8
Allan Chau <allan.chau@icloud.com>
2018-05-14 14:46:28 +0930

    Make sure unhandled promise errors exit the node process
    Signed-off-by: Allan Chau <allan.chau@icloud.com>

--- idearium-lib/common/apm.js
+++ idearium-lib/common/apm.js
@@ -2,8 +2,10 @@
 
 const apm = require('elastic-apm-node');
 const config = require('./config');
+const exception = require('./exception');
 
 const ignoreUrls = (config.get('elasticApmIgnoreUrls') || '').split(',');
+const logLevel = config.get('elasticApmLogLevel') || 'debug';
 
 // Set some defaults.
 if (!ignoreUrls.length) {
@@ -13,10 +15,13 @@ if (!ignoreUrls.length) {
 
 }
 
-apm.start({ ignoreUrls });
+apm.start({
+    ignoreUrls,
+    logLevel,
+});
 
-process.on('unhandledRejection', err => apm.captureError(err));
+process.on('unhandledRejection', err => apm.captureError(err, exception));
 
-apm.handleUncaughtExceptions(require('./exception'));
+apm.handleUncaughtExceptions(exception);
 
 module.exports = apm;
