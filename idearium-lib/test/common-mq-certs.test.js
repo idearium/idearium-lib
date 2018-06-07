3d0fca69ccf1c1d3a7a06fdc5bba47cdf97d6076
Allan Chau <allan.chau@icloud.com>
2018-06-07 16:11:35 +0930

    Add mq certs tests

---
+++ idearium-lib/test/common-mq-certs.test.js
@@ -0,0 +1,48 @@
+/* eslint-env node, mocha */
+
+'use strict';
+
+const { makeConfigs } = require('./util');
+
+let path = require('path'),
+    copy = require('copy-dir'),
+    rimraf = require('rimraf'),
+    dir = path.resolve(__dirname);
+
+describe('common/mq/certs', function () {
+
+    // This is run after common-config and will have therefore cached the config from the previous test.
+    // Set the mqUrl value as common/mq/client uses it.
+    beforeAll(done => makeConfigs()
+        .then(() => {
+
+            // Move the test files into place
+            copy(path.resolve(dir, 'data', 'mq-certs'), path.join(dir, '..', 'mq-certs', process.env.NODE_ENV), done);
+
+        }));
+
+    it('will load the certificates, specific to environment', (done) => {
+
+        require('../common/mq/certs')
+            .then((optsCerts) => {
+
+                expect(Object.keys(optsCerts).sort()).toEqual(['ca', 'cert', 'key']);
+
+                expect(optsCerts.key instanceof Buffer).toBe(true);
+                expect(optsCerts.cert instanceof Buffer).toBe(true);
+                expect(Array.isArray(optsCerts.ca)).toBe(true);
+
+                return done();
+
+            })
+            .catch(done);
+
+    });
+
+    afterAll((done) => {
+
+        rimraf(path.join(dir, '..', 'mq-certs'), done);
+
+    });
+
+});
