50b5b786b3f01b0ea3da44bf6d911d40a11fba87
Allan Chau <allan.chau@icloud.com>
2018-06-26 08:31:59 +0930

    Make session use the same redis client

--- idearium-lib/common/session.js
+++ idearium-lib/common/session.js
@@ -3,10 +3,11 @@
 const config = require('./config');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
+const client = require('./redis');
 
 module.exports = session({
     resave: false,
     saveUninitialized: false,
     secret: config.get('sessionSecret'),
-    store: new RedisStore({ url: config.get('cacheUrl') }),
+    store: new RedisStore({ client }),
 });
