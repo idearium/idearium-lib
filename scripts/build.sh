e5d2d9cda4a7a76dcdf70374a87abbe4b1989ebf
Allan Chau <allan.chau@icloud.com>
2018-06-29 14:02:07 +0930

    Split setup and build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-# Copy over the files to test
-rm -rf root
-mkdir -p $PWD/build/root/app/
-rsync -a $PWD/. $PWD/build/root/app/ --exclude .git --exclude build --exclude node_modules
-
 # Build image.
 docker build -t idearium-lib $PWD/build
