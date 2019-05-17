55ab0480472ffeadabfc1457e84c1b1f9398974f
Allan Chau <allan.chau@icloud.com>
2019-05-17 15:41:18 +0930

    add logger

--- packages/idearium-log/bin/remote.js
+++ packages/idearium-log/bin/remote.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
+'use strict';
 
 const Logger = require('r7insight_node');
 const pump = require('pump');
 const split = require('split2');
 
+/* eslint-disable no-process-env */
 const region = process.env.INSIGHT_OPS_REGION;
 const token = process.env.INSIGHT_OPS_TOKEN;
+/* eslint-enable no-process-env */
 
 if (!region) {
     throw new Error('A region is required!');
--- packages/idearium-log/index.js
+++ packages/idearium-log/index.js
@@ -2,21 +2,25 @@
 
 const pino = require('pino');
 
+let redactPaths = [
+    '*.member.password',
+    '*.password',
+    '*.user.password',
+    'password',
+];
+
+/* eslint-disable no-process-env */
+if (process.env.PINO_REDACT_PATHS) {
+    redactPaths = redactPaths.concat(process.env.PINO_REDACT_PATHS.split(','));
+}
+
 const logger = pino({
     enabled: process.env.LOG_ENABLED !== 'false',
     level: process.env.LOG_LEVEL || 'info',
     prettyPrint: process.env.LOG_REMOTE !== 'true' && process.env.PINO_PRETTY_PRINT !== 'false',
-    redact: [
-        '*.key',
-        '*.member.password',
-        '*.password',
-        '*.token',
-        '*.user.password',
-        'key',
-        'password',
-        'token',
-    ],
+    redact: redactPaths,
     serializers: pino.stdSerializers,
 });
+/* eslint-enable no-process-env */
 
 module.exports = logger.child({ context: require.main.filename });
