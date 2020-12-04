4c2b0c14cd4cdcdfb4d6710bf47c3b236b68975b
allanchau <allan.chau@icloud.com>
2020-12-04 14:19:55 +0930

    remove null result from safePromise

--- packages/utils/safe-promise.js
+++ packages/utils/safe-promise.js
@@ -3,7 +3,7 @@
 const safePromise = (p) =>
     new Promise((resolve) => {
         p.then((result) => resolve([null, result])).catch((err) =>
-            resolve([err, null])
+            resolve([err])
         );
     });
 
