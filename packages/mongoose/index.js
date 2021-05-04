688375a3ee8f0ab4a2ab5398f6fb9208d72e9659
Allan Chau <allan.chau@icloud.com>
2021-05-04 12:18:53 +0930

    update mongoose

--- packages/mongoose/index.js
+++ packages/mongoose/index.js
@@ -29,42 +29,48 @@ const getOptions = (opts = {}) => {
 };
 
 const getDbInfo = (connection) => ({
-    db: `${connection.host}:${connection.port}`
+    db: `${connection.host}:${connection.port}/${connection.name}`
 });
 
-const connect = async (uri, opts = {}) => {
+const connect = async ({ mongoose, options = {}, uri } = {}) => {
     if (!uri) {
         throw new Error(`uri must be provided`);
     }
 
-    const options = getOptions(opts);
-    const { mongoose } = options;
+    if (!mongoose) {
+        throw new Error('You must provide a mongoose instance.');
+    }
+
+    const opts = getOptions(options);
 
     setupMongoose(mongoose);
 
     log.info('Connecting to the database...');
 
-    const connection = await mongoose.connect(uri, options);
+    const connection = await mongoose.connect(uri, opts);
 
     log.info(getDbInfo(connection), 'Connected to the database');
 
     return mongoose;
 };
 
-const createConnections = async (uris, opts = {}) => {
+const createConnections = async ({ mongoose, options = {}, uris } = {}) => {
     if (!uris) {
         throw new Error(`uris array must be provided`);
     }
 
-    const options = getOptions(opts);
-    const { mongoose } = options;
+    if (!mongoose) {
+        throw new Error('You must provide a mongoose instance.');
+    }
+
+    const opts = getOptions(options);
 
     setupMongoose(mongoose);
 
     log.info('Creating connections to the databases...');
 
     const connections = await Promise.all(
-        uris.map((uri) => mongoose.createConnection(uri, options))
+        uris.map((uri) => mongoose.createConnection(uri, opts))
     );
 
     log.info(
