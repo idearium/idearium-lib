d845f2e3c43ce3bc87aa42e41a2372ab523b16cb
Scott Mebberson <scott@scottmebberson.com>
2021-06-11 20:45:43 +0930

    Fixed a potential XSS issue.

--- packages/log-http/middleware/not-found.js
+++ packages/log-http/middleware/not-found.js
@@ -1,4 +1,6 @@
 'use strict';
 
+const escape = require('escape-html');
+
 module.exports = () => (req, res) =>
-    res.status(404).end(`Cannot ${req.method} ${req.url}`);
+    res.status(404).end(`Cannot ${escape(req.method)} ${escape(req.url)}`);
