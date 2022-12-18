fbed114378a994bf43c2c21182019eea5384fa3b
Scott Mebberson <74628+smebberson@users.noreply.github.com>
2022-12-18 19:42:10 +1030

    Fixed an issue with @idearium/log when building with Next.js.

--- packages/log/index.js
+++ packages/log/index.js
@@ -5,20 +5,23 @@ const defaults = require('./defaults');
 
 const sourceLocationKey = 'logging.googleapis.com/sourceLocation';
 
+const getFile = () =>
+    Error()
+        .stack.split('\n')
+        .slice(2)
+        .filter(
+            (s) =>
+                !s.includes('node_modules/pino') &&
+                !s.includes('node_modules\\pino')
+        )[0]
+        .match(/\((.*?):/)[1];
+
 module.exports = (options = {}) =>
     pino(
         Object.assign({}, defaults, options),
         options.stream || pino.destination(1)
     ).child({
         [`${sourceLocationKey}`]: options.sourceLocation || {
-            file: Error()
-                .stack.split('\n')
-                .slice(2)
-                .filter(
-                    (s) =>
-                        !s.includes('node_modules/pino') &&
-                        !s.includes('node_modules\\pino')
-                )[0]
-                .match(/\((.*?):/)[1]
-        }
+            file: getFile(),
+        },
     });
