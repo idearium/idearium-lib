febbabf050c00e55d7676c4f0bd1757dbaa70a44
Scott Mebberson <74628+smebberson@users.noreply.github.com>
2023-03-09 12:51:45 +1030

    Refactored the tests to use a mock per test.

--- packages/certs/tests/index.test.js
+++ packages/certs/tests/index.test.js
@@ -5,7 +5,7 @@ const { loadCerts, loadOsCerts } = require('../');
 
 jest.mock('fs');
 
-beforeEach(() => {
+it('will load ca certs', async () => {
     require('fs').__setMockFiles({
         '/ssl/ca/one.crt': 'ca-one-data',
         '/ssl/ca/two.crt': 'ca-two-data',
@@ -14,20 +14,7 @@ beforeEach(() => {
         '/ssl/mq.key': 'ssl-mq-key-data',
         '/ssl/redis.crt': 'redis-crt-data',
         '/ssl/redis.key': 'redis-key-data',
-        '/etc/ca-certificates.conf': `# test files${os.EOL}/usr/share/ca-certificates/one-cert.crt${os.EOL}/usr/share/ca-certificates/two-cert.crt`,
-        '/usr/share/ca-certificates/one-cert.crt': 'one-cert-content',
-        '/usr/share/ca-certificates/two-cert.crt': 'two-cert-content',
-        '/certs/mq.crt': 'certs-mq-crt-data',
-        '/certs/mq.key': 'certs-mq-key-data',
-        '/tls/redis.crt': 'tls-redis-crt-data',
-        '/tls/mq.key': 'tls-mq-key-data',
-        '/tls/mq.txt': 'tls-mq-txt-data',
-        '/tls/ca/ca-one.crt': 'tls-ca-one-data',
-        '/tls/ca/ca-two.crt': 'tls-ca-two-data',
     });
-});
-
-it('will load ca certs', async () => {
     const certs = await loadCerts();
     expect(certs).toHaveProperty('ca', [
         'ca-one-data',
@@ -36,7 +23,30 @@ it('will load ca certs', async () => {
     ]);
 });
 
+it('will load ca certs', async () => {
+    require('fs').__setMockFiles({
+        '/ssl/ca/a.crt': 'ca-a-data',
+        '/ssl/ca/b.crt': 'ca-b-data',
+        '/ssl/ca/c.crt': 'ca-c-data',
+        '/ssl/mq.crt': 'ssl-mq-crt-data',
+        '/ssl/mq.key': 'ssl-mq-key-data',
+        '/ssl/redis.crt': 'redis-crt-data',
+        '/ssl/redis.key': 'redis-key-data',
+    });
+    const certs = await loadCerts();
+    expect(certs).toHaveProperty('ca', ['ca-a-data', 'ca-b-data', 'ca-c-data']);
+});
+
 it('will load custom certs', async () => {
+    require('fs').__setMockFiles({
+        '/ssl/ca/one.crt': 'ca-one-data',
+        '/ssl/ca/two.crt': 'ca-two-data',
+        '/ssl/ca/three.crt': 'ca-three-data',
+        '/ssl/mq.crt': 'ssl-mq-crt-data',
+        '/ssl/mq.key': 'ssl-mq-key-data',
+        '/ssl/redis.crt': 'redis-crt-data',
+        '/ssl/redis.key': 'redis-key-data',
+    });
     const certs = await loadCerts();
     expect(certs).toHaveProperty('ca', [
         'ca-one-data',
@@ -50,6 +60,21 @@ it('will load custom certs', async () => {
 });
 
 it('will load only key and crt files', async () => {
+    require('fs').__setMockFiles({
+        '/ssl/ca/one.crt': 'ca-one-data',
+        '/ssl/ca/two.crt': 'ca-two-data',
+        '/ssl/ca/three.crt': 'ca-three-data',
+        '/ssl/mq.crt': 'ssl-mq-crt-data',
+        '/ssl/mq.key': 'ssl-mq-key-data',
+        '/ssl/redis.crt': 'redis-crt-data',
+        '/ssl/redis.key': 'redis-key-data',
+        '/etc/ca-certificates.conf': `# test files${os.EOL}/usr/share/ca-certificates/one-cert.crt${os.EOL}/usr/share/ca-certificates/two-cert.crt`,
+        '/tls/redis.crt': 'tls-redis-crt-data',
+        '/tls/mq.key': 'tls-mq-key-data',
+        '/tls/mq.txt': 'tls-mq-txt-data',
+        '/tls/ca/ca-one.crt': 'tls-ca-one-data',
+        '/tls/ca/ca-two.crt': 'tls-ca-two-data',
+    });
     const certs = await loadCerts('/tls');
     expect(certs).toHaveProperty('ca', ['tls-ca-one-data', 'tls-ca-two-data']);
     expect(certs).toHaveProperty('certs', {
@@ -59,15 +84,24 @@ it('will load only key and crt files', async () => {
 });
 
 it("will return an empty ca array if the ca directory doesn't exist", async () => {
-    const certs = await loadCerts('/certs');
+    require('fs').__setMockFiles({
+        '/ssl/mq.crt': 'mq-crt-data',
+        '/ssl/mq.key': 'mq-key-data',
+    });
+    const certs = await loadCerts();
 
     expect(certs).toHaveProperty('ca', []);
     expect(certs).toHaveProperty('certs', {
-        mq: { crt: 'certs-mq-crt-data', key: 'certs-mq-key-data' },
+        mq: { crt: 'mq-crt-data', key: 'mq-key-data' },
     });
 });
 
 it('will load OS provided certs', async () => {
+    require('fs').__setMockFiles({
+        '/etc/ca-certificates.conf': `# test files${os.EOL}/usr/share/ca-certificates/one-cert.crt${os.EOL}/usr/share/ca-certificates/two-cert.crt`,
+        '/usr/share/ca-certificates/one-cert.crt': 'one-cert-content',
+        '/usr/share/ca-certificates/two-cert.crt': 'two-cert-content',
+    });
     const certs = await loadOsCerts();
 
     expect(certs).toEqual(['one-cert-content', 'two-cert-content']);
