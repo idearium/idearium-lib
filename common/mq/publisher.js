d46841f131f0a59405dcfd7553999feb7692e8ab
Allan Chau <allan.chau@icloud.com>
2024-12-13 09:54:01 +0930

    reduce logging

--- common/mq/publisher.js
+++ common/mq/publisher.js
@@ -12,8 +12,8 @@ const log = require('@idearium/log')();
 const publish = (type, data) => {
 
     return manager.publish(type, data)
-        .then(() => log.debug({ data, type }, `Published message of type: ${type}`))
-        .catch(err => log.error({ err }, `Could not publish message of type: ${type}`));
+        .then(() => log.trace({ data, type }, `Published message of type: ${type}`))
+        .catch(err => log.warn({ err }, `Could not publish message of type: ${type}`));
 
 };
 
